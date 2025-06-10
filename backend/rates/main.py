bdf72d6b3b4987165d89b49929cf64e3178804d3
Varun Chowdary <vrcreddy02@gmail.com>
2025-06-10 11:43:08 -0500

    backend update to main

--- backend/rates/main.py
+++ backend/rates/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from rates import get_yields
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # for local dev
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] temporarily
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
