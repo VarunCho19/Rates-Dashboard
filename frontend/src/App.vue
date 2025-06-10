60598388d42116a492cf2993d7a4022f15b70ce3
Varun Chowdary <vrcreddy02@gmail.com>
2025-06-10 12:05:28 -0500

    poc frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import YieldCurve from './components/YieldCurve.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <HelloWorld msg="Welcome to the Yield Curve Dashboard" />
+    <YieldCurve />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ frontend/src/components/YieldCurve.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-xl font-bold mb-4">U.S. Treasury Yield Curve</h2>
+    <Line v-if="chartData" :data="chartData" :options="chartOptions" />
+    <p v-else>Loading...</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Line } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, LineElement,CategoryScale, PointElement, LinearScale);
+
+
+const chartData = ref(null)
+const chartOptions = ref({
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Yield Curve',
+    },
+  },
+});
+
+const fetchYieldCurveData = async () => {
+  try {
+    const response = await fetch('https://rates-dashboard.onrender.com/yields')
+    const data = await response.json()
+    console.log('Yield Curve Data Recevied:', data);
+
+    const labels = Object.keys(data)
+    const values = Object.values(data)
+
+    chartData.value = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Yield Curve',
+          backgroundColor: '#4B9CD3',
+          borderColor: '#4B9CD3',
+          data: values,
+          fill: false,
+          tension: 0.3,
+        }
+      ]
+    };
+    console.log('Yield Curve Data:', chartData.value);
+
+  }
+    catch (error) {
+        console.error('Error fetching yield curve data:', error);
+    }
+};
+
+onMounted(() => {
+    fetchYieldCurveData();
+});
+</script>
+
+<style scoped>
+
+</style>
